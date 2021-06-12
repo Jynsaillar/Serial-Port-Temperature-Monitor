f1f6412323d1ebdf92cb360d1b21c7390cf9176d
Jyn Saillar <cieldaron1@yahoo.de>
2021-06-12 18:08:13 +0200

    Add project files.

---
+++ SerialHelpers/SerialHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.IO.Ports;
+
+namespace Serial_Port_Temperature_Monitor.SerialHelpers
+{
+    public class SerialHelper
+    {
+        private
+        public SerialHelper()
+        {
+        }
+    }
+}
