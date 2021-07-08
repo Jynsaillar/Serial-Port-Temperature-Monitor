fb2788f77d4b8b3abaf59841ad65d4f45b636d1c
Jyn Saillar <cieldaron1@yahoo.de>
2021-07-08 15:47:43 +0200

    Added code to main form with basic serial port UI elements, added constructor to pass serial helper class from main program to form

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Serial_Port_Temperature_Monitor.SerialHelpers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,12 +11,54 @@ using System.Windows.Forms;
 
 namespace Serial_Port_Temperature_Monitor
 {
-    public partial class Form1 : Form
+    public partial class FormSerialMonitor : Form
     {
-        public Form1()
+        private SerialHelper _serialHelper { get; set; }
+
+        public FormSerialMonitor()
         {
             InitializeComponent();
         }
 
+        private void LoadUiSettings()
+        {
+            comboBoxBaudRate.SelectedIndex = 6;
+            DiscoverSerialPorts();
+        }
+
+        private void DiscoverSerialPorts()
+        {
+            comboBoxSerialPorts.Items.Clear();
+
+            foreach (var port in _serialHelper.GetAvailablePorts())
+            {
+                comboBoxSerialPorts.Items.Add(port);
+            }
+
+            if (comboBoxSerialPorts.Items.Count > 0)
+            {
+                comboBoxSerialPorts.SelectedIndex = 0;
+            }
+        }
+
+        public FormSerialMonitor(SerialHelper serialHelper) : this()
+        {
+            _serialHelper = serialHelper;
+            LoadUiSettings();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DiscoverSerialPorts();
+        }
+
+        private void comboBoxBaudRate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxBaudRate.SelectedItem == null)
+                return;
+
+            _serialHelper.SetBaudRate(
+                int.Parse(comboBoxBaudRate.SelectedItem.ToString()));
+        }
     }
 }
