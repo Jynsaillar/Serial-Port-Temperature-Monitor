570fb4469c7f43eeaf43978d56c4ff06cce84479
Jyn Saillar <cieldaron1@yahoo.de>
2021-07-08 15:49:15 +0200

    Added functions to set read/write timeout, added basic threading, added queue flushing for serial history

--- SerialHelpers/SerialHelper.cs
+++ SerialHelpers/SerialHelper.cs
@@ -4,6 +4,8 @@ using System.Text;
 using System.Threading;
 using System.IO.Ports;
 using System.Linq;
+using System.Drawing.Drawing2D;
+using System.Diagnostics;
 
 namespace Serial_Port_Temperature_Monitor.SerialHelpers
 {
@@ -13,6 +15,7 @@ namespace Serial_Port_Temperature_Monitor.SerialHelpers
         public SerialPort SerialPort { get; private set; }
         private bool _continueListening { get; set; }
         private List<string> _serialQueue { get; set; }
+        private int _historyLimit { get; set; }
 
         private Thread _readThread { get; set; }
         private StringComparer _stringComparer = StringComparer.OrdinalIgnoreCase;
@@ -21,9 +24,11 @@ namespace Serial_Port_Temperature_Monitor.SerialHelpers
         {
             SerialPort = new SerialPort();
             _continueListening = false;
+            _historyLimit = 10000; // Default cap for messages in serial message queue
             _readThread = new Thread(Read);
             // Set default values
             SerialPort.BaudRate = 9600;
+            SetPortTimeouts(500, 500);
         }
 
         public string[] GetAvailablePorts()
@@ -40,6 +45,12 @@ namespace Serial_Port_Temperature_Monitor.SerialHelpers
                 {
                     string message = SerialPort.ReadLine();
                     _serialQueue.Add(message);
+
+                    // Flush serial history once a manually set number of entries have been received
+                    if (_serialQueue.Count >= _historyLimit)
+                    {
+                        _serialQueue.Clear();
+                    }
                 }
                 catch (TimeoutException)
                 {
@@ -81,6 +92,41 @@ namespace Serial_Port_Temperature_Monitor.SerialHelpers
         public void SetPortHandshake(Handshake handshake)
         {
             SerialPort.Handshake = handshake;
+        }
+
+        public void SetPortReadTimeOut(int readTimeout)
+        {
+            SerialPort.ReadTimeout = readTimeout;
+        }
+
+        public void SetPortWriteTimeout(int writeTimeout)
+        {
+            SerialPort.WriteTimeout = writeTimeout;
+        }
+
+        public void SetPortTimeouts(int readTimeout, int writeTimeout)
+        {
+            SetPortReadTimeOut(readTimeout);
+            SetPortWriteTimeout(writeTimeout);
+        }
+
+        public void StartListening()
+        {
+            _continueListening = true;
+            if (_readThread.IsAlive)
+            {
+                // Todo
+                Trace.TraceInformation("Attempted to start an already existing thread.");
+
+                return;
+            }
+
+            _readThread.Start();
+        }
+
+        public void StopListening()
+        {
+            _continueListening = false;
         }
     }
 }
