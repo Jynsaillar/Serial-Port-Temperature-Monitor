d950c67c9a5114d07e4807f9f3aae1ca13ffb557
Jynsaillar <Jynsaillar@ymail.com>
2021-11-14 16:52:06 +0100

    Added a monitor window to the UI, a logging function and a logging table view

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,17 +29,35 @@ namespace Serial_Port_Temperature_Monitor
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("");
             this.buttonRefreshSerialPorts = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonSerialMonitorSend = new System.Windows.Forms.Button();
+            this.textBoxSerialMonitorSend = new System.Windows.Forms.TextBox();
+            this.richTextBoxSerialMonitor = new System.Windows.Forms.RichTextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.comboBoxSerialPorts = new System.Windows.Forms.ComboBox();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.comboBoxBaudRate = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonSerialPortConnect = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.buttonParseData = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonRefreshSerialPorts
@@ -50,16 +68,46 @@ namespace Serial_Port_Temperature_Monitor
             this.buttonRefreshSerialPorts.TabIndex = 0;
             this.buttonRefreshSerialPorts.Text = "Refresh";
             this.buttonRefreshSerialPorts.UseVisualStyleBackColor = true;
-            this.buttonRefreshSerialPorts.Click += new System.EventHandler(this.button1_Click);
+            this.buttonRefreshSerialPorts.Click += new System.EventHandler(this.buttonRefreshSerialPorts_Click);
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.buttonSerialMonitorSend);
+            this.groupBox1.Controls.Add(this.textBoxSerialMonitorSend);
+            this.groupBox1.Controls.Add(this.richTextBoxSerialMonitor);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(558, 296);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Serial Monitor";
+            // 
+            // buttonSerialMonitorSend
+            // 
+            this.buttonSerialMonitorSend.Location = new System.Drawing.Point(463, 267);
+            this.buttonSerialMonitorSend.Name = "buttonSerialMonitorSend";
+            this.buttonSerialMonitorSend.Size = new System.Drawing.Size(89, 23);
+            this.buttonSerialMonitorSend.TabIndex = 2;
+            this.buttonSerialMonitorSend.Text = "Send";
+            this.buttonSerialMonitorSend.UseVisualStyleBackColor = true;
+            this.buttonSerialMonitorSend.Click += new System.EventHandler(this.buttonSerialMonitorSend_Click);
+            // 
+            // textBoxSerialMonitorSend
+            // 
+            this.textBoxSerialMonitorSend.Location = new System.Drawing.Point(7, 267);
+            this.textBoxSerialMonitorSend.Name = "textBoxSerialMonitorSend";
+            this.textBoxSerialMonitorSend.Size = new System.Drawing.Size(450, 23);
+            this.textBoxSerialMonitorSend.TabIndex = 1;
+            // 
+            // richTextBoxSerialMonitor
+            // 
+            this.richTextBoxSerialMonitor.Location = new System.Drawing.Point(7, 23);
+            this.richTextBoxSerialMonitor.Name = "richTextBoxSerialMonitor";
+            this.richTextBoxSerialMonitor.ReadOnly = true;
+            this.richTextBoxSerialMonitor.Size = new System.Drawing.Size(545, 238);
+            this.richTextBoxSerialMonitor.TabIndex = 0;
+            this.richTextBoxSerialMonitor.Text = "";
+            this.richTextBoxSerialMonitor.TextChanged += new System.EventHandler(this.richTextBoxSerialMonitor_TextChanged);
             // 
             // groupBox2
             // 
@@ -80,6 +128,7 @@ namespace Serial_Port_Temperature_Monitor
             this.comboBoxSerialPorts.Name = "comboBoxSerialPorts";
             this.comboBoxSerialPorts.Size = new System.Drawing.Size(123, 23);
             this.comboBoxSerialPorts.TabIndex = 1;
+            this.comboBoxSerialPorts.SelectedIndexChanged += new System.EventHandler(this.comboBoxSerialPorts_SelectedIndexChanged);
             // 
             // statusStrip1
             // 
@@ -93,6 +142,7 @@ namespace Serial_Port_Temperature_Monitor
             // 
             this.flowLayoutPanel1.Controls.Add(this.groupBox2);
             this.flowLayoutPanel1.Controls.Add(this.groupBox3);
+            this.flowLayoutPanel1.Controls.Add(this.panel1);
             this.flowLayoutPanel1.Location = new System.Drawing.Point(576, 21);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(212, 404);
@@ -133,19 +183,91 @@ namespace Serial_Port_Temperature_Monitor
             this.comboBoxBaudRate.TabIndex = 2;
             this.comboBoxBaudRate.SelectedIndexChanged += new System.EventHandler(this.comboBoxBaudRate_SelectedIndexChanged);
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonSerialPortConnect);
+            this.panel1.Location = new System.Drawing.Point(3, 140);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 264);
+            this.panel1.TabIndex = 4;
+            // 
+            // buttonSerialPortConnect
+            // 
+            this.buttonSerialPortConnect.Enabled = false;
+            this.buttonSerialPortConnect.Location = new System.Drawing.Point(10, 232);
+            this.buttonSerialPortConnect.Name = "buttonSerialPortConnect";
+            this.buttonSerialPortConnect.Size = new System.Drawing.Size(187, 23);
+            this.buttonSerialPortConnect.TabIndex = 0;
+            this.buttonSerialPortConnect.Text = "Connect";
+            this.buttonSerialPortConnect.UseVisualStyleBackColor = true;
+            this.buttonSerialPortConnect.Click += new System.EventHandler(this.buttonSerialPortConnect_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.listView2);
+            this.groupBox4.Controls.Add(this.buttonParseData);
+            this.groupBox4.Controls.Add(this.listView1);
+            this.groupBox4.Location = new System.Drawing.Point(19, 314);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(551, 108);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Data Display";
+            // 
+            // buttonParseData
+            // 
+            this.buttonParseData.Location = new System.Drawing.Point(456, 79);
+            this.buttonParseData.Name = "buttonParseData";
+            this.buttonParseData.Size = new System.Drawing.Size(89, 23);
+            this.buttonParseData.TabIndex = 1;
+            this.buttonParseData.Text = "Parse Data";
+            this.buttonParseData.UseVisualStyleBackColor = true;
+            this.buttonParseData.Click += new System.EventHandler(this.buttonParseData_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem4,
+            listViewItem5,
+            listViewItem6});
+            this.listView1.Location = new System.Drawing.Point(7, 23);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(212, 79);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3});
+            this.listView2.Location = new System.Drawing.Point(225, 22);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(225, 79);
+            this.listView2.TabIndex = 2;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
             // FormSerialMonitor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.groupBox1);
             this.Name = "FormSerialMonitor";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.flowLayoutPanel1.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -161,6 +283,15 @@ namespace Serial_Port_Temperature_Monitor
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.ComboBox comboBoxBaudRate;
+        private System.Windows.Forms.RichTextBox richTextBoxSerialMonitor;
+        private System.Windows.Forms.TextBox textBoxSerialMonitorSend;
+        private System.Windows.Forms.Button buttonSerialMonitorSend;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonSerialPortConnect;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button buttonParseData;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,11 @@
-﻿using Serial_Port_Temperature_Monitor.SerialHelpers;
+﻿using CsvHelper;
+using Serial_Port_Temperature_Monitor.DataHelpers;
+using Serial_Port_Temperature_Monitor.SerialHelpers;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace Serial_Port_Temperature_Monitor
@@ -14,16 +13,89 @@ namespace Serial_Port_Temperature_Monitor
     public partial class FormSerialMonitor : Form
     {
         private SerialHelper _serialHelper { get; set; }
+        private System.Timers.Timer LoggingTimer { get; set; }
 
         public FormSerialMonitor()
         {
             InitializeComponent();
         }
 
+        public FormSerialMonitor(SerialHelper serialHelper) : this()
+        {
+            _serialHelper = serialHelper;
+            _serialHelper.MessageReceived += _serialHelper_MessageReceived;
+            LoadUiSettings();
+            SetupLoggingTimer();
+        }
+
+        private void _serialHelper_MessageReceived(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            UpdateSerialMonitor();
+        }
+
+        private delegate void UpdateSerialMonitorCallback();
+
+        private void UpdateSerialMonitor()
+        {
+            if (richTextBoxSerialMonitor.InvokeRequired)
+            {
+                UpdateSerialMonitorCallback updateSerialMonitorCallback = new UpdateSerialMonitorCallback(UpdateSerialMonitor);
+                this.Invoke(updateSerialMonitorCallback, new object[] { });
+            }
+            else
+            {
+                richTextBoxSerialMonitor.Lines = _serialHelper.SerialQueue.ToArray();
+            }
+        }
+
+        private void SetupLoggingTimer()
+        {
+            LoggingTimer = new System.Timers.Timer();
+            LoggingTimer.Interval = TimeSpan.FromMinutes(1).TotalMilliseconds;
+            LoggingTimer.Elapsed += LoggingTimer_Elapsed;
+        }
+
+        private void LoggingTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            LogToFile();
+        }
+
+        private void LogToFile()
+        {
+            // Populate Sensor1/Sensor2 data in DataHelper
+            DataHelper.ParseSerialData(_serialHelper.SerialQueue.ToList());
+            //todo
+            using (var writer = new StreamWriter("sensor1.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(DataHelper.SensorOneData);
+            }
+
+            using (var writer = new StreamWriter("sensor2.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(DataHelper.SensorTwoData);
+            }
+        }
+
         private void LoadUiSettings()
         {
             comboBoxBaudRate.SelectedIndex = 6;
+            GenerateListView();
             DiscoverSerialPorts();
+        }
+
+        private void GenerateListView()
+        {
+            listView1.Columns.Add("Sensor");
+            listView1.Columns.Add("Temperature");
+            listView1.Columns.Add("Time");
+            listView1.View = View.Details;
+
+            listView2.Columns.Add("Sensor");
+            listView2.Columns.Add("Temperature");
+            listView2.Columns.Add("Time");
+            listView2.View = View.Details;
         }
 
         private void DiscoverSerialPorts()
@@ -41,17 +113,6 @@ namespace Serial_Port_Temperature_Monitor
             }
         }
 
-        public FormSerialMonitor(SerialHelper serialHelper) : this()
-        {
-            _serialHelper = serialHelper;
-            LoadUiSettings();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            DiscoverSerialPorts();
-        }
-
         private void comboBoxBaudRate_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (comboBoxBaudRate.SelectedItem == null)
@@ -59,6 +120,106 @@ namespace Serial_Port_Temperature_Monitor
 
             _serialHelper.SetBaudRate(
                 int.Parse(comboBoxBaudRate.SelectedItem.ToString()));
+        }
+
+        private void buttonSerialMonitorSend_Click(object sender, EventArgs e)
+        {
+            FlushPromptAndSendSerial();
+        }
+
+        private void FlushPromptAndSendSerial()
+        {
+            if (string.IsNullOrWhiteSpace(textBoxSerialMonitorSend.Text))
+                return;
+
+            _serialHelper.Write(textBoxSerialMonitorSend.Text);
+
+            textBoxSerialMonitorSend.Clear();
+        }
+
+        private void buttonSerialPortConnect_Click(object sender, EventArgs e)
+        {
+            if (!_serialHelper.Connected)
+            {
+                richTextBoxSerialMonitor.Clear();
+                ConnectToSerialPort();
+                (sender as Button).Text = "Disconnect";
+            }
+            else
+            {
+                DisconnectFromSerialPort();
+                (sender as Button).Text = "Connect";
+            }
+        }
+
+        private void ConnectToSerialPort()
+        {
+            _serialHelper.StartListening();
+            LoggingTimer.Start();
+        }
+
+        private void DisconnectFromSerialPort()
+        {
+            _serialHelper.StopListening();
+            LoggingTimer.Stop();
+        }
+
+        private void buttonRefreshSerialPorts_Click(object sender, EventArgs e)
+        {
+            DiscoverSerialPorts();
+        }
+
+        private void comboBoxSerialPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (buttonSerialPortConnect.Enabled)
+            {
+                // Disable connect button in case the selected port is invalid so the user can only try to connect if there's a valid port selected
+                buttonSerialPortConnect.Enabled = false;
+            }
+
+            if (comboBoxSerialPorts.SelectedItem == null)
+                return;
+
+            bool portSetSuccessfully = _serialHelper.SetPortName(comboBoxSerialPorts.SelectedItem.ToString());
+
+            if (portSetSuccessfully)
+            {
+                buttonSerialPortConnect.Enabled = true;
+            }
+        }
+
+        private void richTextBoxSerialMonitor_TextChanged(object sender, EventArgs e)
+        {
+            richTextBoxSerialMonitor.SelectionStart = richTextBoxSerialMonitor.Text.Length;
+            richTextBoxSerialMonitor.ScrollToCaret();
+        }
+
+        private void buttonParseData_Click(object sender, EventArgs e)
+        {
+            ParseData();
+        }
+
+        private void ParseData()
+        {
+            listView1.Items.Clear();
+            listView2.Items.Clear();
+
+            DataHelper.ParseSerialData(_serialHelper.SerialQueue.ToList());
+
+            foreach (var sensorData in DataHelper.SensorOneData)
+            {
+                ListViewItem lvi = new ListViewItem(sensorData.AsStringArray());
+                listView1.Items.Add(lvi);
+            }
+
+            foreach (var sensorData in DataHelper.SensorTwoData)
+            {
+                ListViewItem lvi = new ListViewItem(sensorData.AsStringArray());
+                listView2.Items.Add(lvi);
+            }
+
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+            listView2.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
         }
     }
 }
