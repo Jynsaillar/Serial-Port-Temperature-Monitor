48316aa1264d9e225feac5deabd2d0ce52e6313b
Jynsaillar <Jynsaillar@ymail.com>
2021-11-14 16:50:22 +0100

    Changed serial queue to be an OvservableCollection<T> so that we can listen to data transmissions on the serial port, added multiple checks to the methods for opening, reading from and writing to the serial port as well as the listening threads, added method to clear the serial queue history

--- SerialHelpers/SerialHelper.cs
+++ SerialHelpers/SerialHelper.cs
@@ -6,6 +6,8 @@ using System.IO.Ports;
 using System.Linq;
 using System.Drawing.Drawing2D;
 using System.Diagnostics;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 namespace Serial_Port_Temperature_Monitor.SerialHelpers
 {
@@ -14,7 +16,22 @@ namespace Serial_Port_Temperature_Monitor.SerialHelpers
         public string[] AvailablePorts { get; private set; }
         public SerialPort SerialPort { get; private set; }
         private bool _continueListening { get; set; }
-        private List<string> _serialQueue { get; set; }
+        public bool Connected { get; set; }
+        private ObservableCollection<string> _serialQueue;
+        public ObservableCollection<string> SerialQueue => _serialQueue;
+
+        public event NotifyCollectionChangedEventHandler MessageReceived
+        {
+            add
+            {
+                _serialQueue.CollectionChanged += value;
+            }
+            remove
+            {
+                this._serialQueue.CollectionChanged -= value;
+            }
+        }
+
         private int _historyLimit { get; set; }
 
         private Thread _readThread { get; set; }
@@ -22,13 +39,14 @@ namespace Serial_Port_Temperature_Monitor.SerialHelpers
 
         public SerialHelper()
         {
+            _serialQueue = new ObservableCollection<string>();
             SerialPort = new SerialPort();
             _continueListening = false;
             _historyLimit = 10000; // Default cap for messages in serial message queue
-            _readThread = new Thread(Read);
             // Set default values
             SerialPort.BaudRate = 9600;
             SetPortTimeouts(500, 500);
+            SerialPort.DtrEnable = true; // Forces restart on serial connect on devices like Arduino Uno, Mega 2560, ESP32 etc.
         }
 
         public string[] GetAvailablePorts()
@@ -41,10 +59,34 @@ namespace Serial_Port_Temperature_Monitor.SerialHelpers
         {
             while (_continueListening)
             {
+                // Abort if serial connection is interrupted, for example if the user removes the USB while the program is polling for serial data
+                if (!SerialPort.IsOpen)
+                {
+                    Connected = false;
+                    _continueListening = false;
+                    continue;
+                }
                 try
                 {
+                    int bytesToRead = SerialPort.BytesToRead;
+
+                    if (bytesToRead == 0)
+                        continue;
+
+                    /*
+
+                    byte[] buffer = new byte[bytesToRead];
+                    SerialPort.Read(buffer, 0, buffer.Length);
+
+                    //
+                    string message = Encoding.ASCII.GetString(buffer, 0, bytesToRead);
+                    Trace.TraceInformation("msg: " + message);
+                    //
+                    */
+
                     string message = SerialPort.ReadLine();
-                    _serialQueue.Add(message);
+                    if (!string.IsNullOrWhiteSpace(message))
+                        _serialQueue.Add(message.TrimEnd(Environment.NewLine.ToCharArray()) + "," + DateTime.Now + Environment.NewLine);
 
                     // Flush serial history once a manually set number of entries have been received
                     if (_serialQueue.Count >= _historyLimit)
@@ -55,18 +97,35 @@ namespace Serial_Port_Temperature_Monitor.SerialHelpers
                 catch (TimeoutException)
                 {
                 }
+                catch (OperationCanceledException)
+                {
+                }
             }
         }
 
-        public void SetPortName(string portName)
+        public void Write(string message)
+        {
+            if (!TryOpenSerialPort())
+                return;
+
+            SerialPort.Write(message);
+        }
+
+        public bool SetPortName(string portName)
         {
             if (string.IsNullOrWhiteSpace(portName))
-                return;
+                return false;
+
+            if (SerialPort.IsOpen)
+                return false;
 
             if (SerialPort.GetPortNames().Any(item => _stringComparer.Equals(item, portName)))
             {
                 SerialPort.PortName = portName;
+                return true;
             }
+
+            return false;
         }
 
         public void SetBaudRate(int baudRate)
@@ -110,23 +169,55 @@ namespace Serial_Port_Temperature_Monitor.SerialHelpers
             SetPortWriteTimeout(writeTimeout);
         }
 
+        private void CreateReadThread()
+        {
+            if (_readThread == null || !_readThread.IsAlive)
+                _readThread = new Thread(Read);
+        }
+
         public void StartListening()
         {
-            _continueListening = true;
-            if (_readThread.IsAlive)
-            {
-                // Todo
-                Trace.TraceInformation("Attempted to start an already existing thread.");
-
+            if (!TryOpenSerialPort())
                 return;
-            }
+
+            CreateReadThread();
+
+            Connected = true;
+            _continueListening = true;
 
             _readThread.Start();
         }
 
         public void StopListening()
         {
+            Connected = false;
             _continueListening = false;
+            SerialPort.Close();
+        }
+
+        private bool TryOpenSerialPort()
+        {
+            if (!SerialPort.IsOpen)
+            {
+                try
+                {
+                    SerialPort.Open();
+                }
+                catch (Exception ex)
+                {
+                    Trace.TraceError("Exception thrown while opening port: "
+                       + "\n  Type:    " + ex.GetType().Name
+                       + "\n  Message: " + ex.Message);
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public void ClearHistory()
+        {
+            _serialQueue.Clear();
         }
     }
 }
